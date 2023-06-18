50cff67128a94752764914d3131df7972552f109
jessejp <jessepernila@gmail.com>
2023-06-18 19:04:27 +0300

    feat: simple timer logic

--- src/interface/Timer.tsx
+++ src/interface/Timer.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { usePomodoro } from "../store/usePomodoro";
 
 const Timer = () => {
-  return (
-    <div>Timer</div>
-  )
-}
+  const [minutes, setMinutes] = useState(0.1);
+  const { start, stop, addRound, currentRound, rounds, isRunning, startTime } =
+    usePomodoro();
+  const timerRef = useRef<HTMLDivElement>(null);
 
-export default Timer
+  useEffect(() => {
+    let intervalId: number | undefined;
+
+    if (isRunning) {
+      timerRef.current!.innerText = "0:00";
+      intervalId = setInterval(() => {
+        // Calculate the elapsed time
+        const currentTime = Date.now();
+        const elapsedTime = Math.floor((currentTime - startTime) / 1000);
+        console.log(elapsedTime);
+        const seconds = elapsedTime % 60;
+        timerRef.current!.innerText = `${Math.floor(elapsedTime / 60)}:${
+          seconds < 10 ? "0" : ""
+        }${seconds}`;
+        if (elapsedTime >= 60 * minutes) {
+          // 25 minutes have passed
+          console.log("timer passed", currentRound, rounds);
+          if (currentRound >= rounds) stop();
+        }
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning, startTime, stop]);
+
+  console.log("dom rendered");
+
+  return (
+    <>
+      <div className="text-5xl font-bold text-center" ref={timerRef}>
+        0:00
+      </div>
+      <div className="mt-4" />
+      {!isRunning && (
+        <button
+          className="bg-pink-300 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            start({ rounds: 1 });
+            addRound();
+          }}
+        >
+          Start {minutes} minutes
+        </button>
+      )}
+      {!!isRunning && (
+        <button
+          className="bg-pink-500 hover:bg-pink-400 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            stop();
+          }}
+        >
+          Stop
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Timer;
--- src/store/usePomodoro.tsx
+++ src/store/usePomodoro.tsx
@@ -1,8 +1,37 @@
 import { create } from "zustand";
 
-export const usePomodoro = create((set) => {
+interface PomodoroState {
+  startTime: number;
+  workTime: number;
+  rounds: number;
+  currentRound: number;
+  isRunning: boolean;
+  start: (params: { rounds: number }) => void;
+  stop: () => void;
+  addRound: () => void;
+}
+
+export const usePomodoro = create<PomodoroState>((set) => {
   return {
-    timer: 0,
-    
+    startTime: 0,
+    workTime: 0,
+    rounds: 1,
+    currentRound: 0,
+    isRunning: false,
+
+    start: ({ rounds }) =>
+      set(() => {
+        console.log(rounds);
+
+        return {
+          isRunning: true,
+          startTime: Date.now(),
+          currentRound: 0,
+          rounds,
+        };
+      }),
+    stop: () => set(() => ({ isRunning: false })),
+    addRound: () =>
+      set(({ currentRound }) => ({ currentRound: currentRound + 1 })),
   };
 });
