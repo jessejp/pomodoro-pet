8a748696e09855c38c66153bd57ee6b0d947cd9a
jessejp <jessepernila@gmail.com>
2023-08-24 16:00:31 +0300

    added radio input for selecting current session's task

--- src/interface/SessionLog.tsx
+++ src/interface/SessionLog.tsx
@@ -16,23 +16,44 @@ const SessionLog = () => {
 
   if (!isRunning) return null;
   return (
-    <div className="fixed bottom-1/3 right-5 z-10 w-80 rounded border-4 border-violet-700 bg-orangeFlavour px-4 py-4">
+    <div className="fixed bottom-1/3 right-5 z-10 w-96 rounded border-4 border-violet-700 bg-orangeFlavour px-4 py-4">
+      <div className="absolute left-4 top-4 text-2xl">📝</div>
       <h3 className="text-2xl font-bold text-violet-700">Session Log</h3>
       <div className="mt-4" />
-      <div className="flex flex-col gap-1 rounded bg-white p-2">
-        <div className="flex bg-neutral-100 text-left">
+      <div className="flex h-fit max-h-80 flex-col gap-1 overflow-y-scroll rounded bg-white p-2">
+        <div className="flex bg-white py-1 text-left text-neutral-500">
+          <div className="w-14">Time Done</div>
+          <div className="w-48">Task</div>
+          <div className="w-14">Current Session</div>
+        </div>
+        <div className="flex bg-neutral-100 py-1 text-left">
           <div className="w-14">0.00 h</div>
           <textarea
             placeholder="Current Session"
             className="w-48 border-2 border-neutral-400 bg-neutral-50 px-1"
           />
+          <div className="grid w-14 place-content-center">
+            <input
+              id="active-session-new"
+              type="radio"
+              name="active-session"
+              defaultChecked
+            />
+          </div>
         </div>
-        {DUMMY_sessionlist.map((entry) => (
-          <div className="flex text-left odd:bg-neutral-100">
+        {DUMMY_sessionlist.map((entry, index) => (
+          <div className="flex py-1 text-left odd:bg-neutral-100">
             <div className="w-14">
               {Math.round((entry.minutes / 60) * 100) / 100} h
             </div>
             <div className="w-48">{entry.message}</div>
+            <div className="grid w-14 place-content-center">
+              <input
+                id={`active-session-${index}`}
+                type="radio"
+                name="active-session"
+              />
+            </div>
           </div>
         ))}
       </div>
