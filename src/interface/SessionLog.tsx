e51896c1dc86e4212da1fdff76fa4a5a5fb5af9e
jessejp <jessepernila@gmail.com>
2023-08-29 22:55:04 +0300

    Added zustand store for session log

--- src/interface/SessionLog.tsx
+++ src/interface/SessionLog.tsx
@@ -1,19 +1,24 @@
+import { useEffect, useState } from "react";
 import { usePomodoro } from "../utils/usePomodoro";
+import { useSessionLog } from "../utils/useSessionLog";
 import Draggable from "./components/Draggable";
 
-const DUMMY_sessionlist = [
-  { message: "Developing Pomodoro Pets", minutes: 85 },
-  { message: "Practising Piano", minutes: 60 },
-  { message: "Really long task name that I hadasessionfor", minutes: 30 },
-  {
-    message:
-      "JIRATIK-4143 Develop new session log for pomodoro pets to track time and tasks",
-    minutes: 140,
-  },
-];
-
 const SessionLog = () => {
+  const [taskMessageInput, setTaskMessageInput] = useState<string | null>("");
+  const [showTimeWithBreaks, setShowTimeWithBreaks] = useState<boolean>(false);
+
+  const {
+    sessionLog,
+    newTaskMessage,
+    selectedTaskIndex,
+    updateSelectedTaskIndex,
+    updateNewTaskMessage,
+  } = useSessionLog();
   const { pomodoroPhase } = usePomodoro();
+
+  useEffect(() => {
+    updateNewTaskMessage(taskMessageInput);
+  }, [taskMessageInput, updateNewTaskMessage]);
 
   if (pomodoroPhase === "none") return null;
   return (
@@ -26,40 +31,67 @@ const SessionLog = () => {
         <div className="mt-4" />
         <div className="flex h-fit max-h-80 flex-col gap-1 overflow-y-scroll rounded bg-white p-2">
           <div className="flex bg-white py-1 text-left text-neutral-500">
-            <div className="w-14">Time Done</div>
+            <div className="w-14">Total Time</div>
             <div className="w-48">Task</div>
             <div className="w-14">Current Session</div>
           </div>
           <div className="flex bg-neutral-100 py-1 text-left">
             <div className="w-14">0.00 h</div>
             <textarea
-              placeholder="Current Session"
+              value={!newTaskMessage ? "" : newTaskMessage}
+              onChange={(event) => {
+                setTaskMessageInput(event.target.value);
+              }}
+              placeholder="New Task"
               className="w-48 border-2 border-neutral-400 bg-neutral-50 px-1"
             />
             <div className="grid w-14 place-content-center">
               <input
-                id="active-session-new"
+                className="scale-125"
+                id="active-session--1"
                 type="radio"
                 name="active-session"
-                defaultChecked
+                checked={selectedTaskIndex === -1}
+                onChange={() => {
+                  updateSelectedTaskIndex(-1);
+                }}
               />
             </div>
           </div>
-          {DUMMY_sessionlist.map((entry, index) => (
+          {sessionLog.map((entry, index) => (
             <div key={index} className="flex py-1 text-left odd:bg-neutral-100">
               <div className="w-14">
-                {Math.round((entry.minutes / 60) * 100) / 100} h
+                {!showTimeWithBreaks &&
+                  `${Math.round((entry.minutes / 60) * 100) / 100} h`}
+                {!!showTimeWithBreaks &&
+                  `${Math.round((entry.minutesWithBreaks / 60) * 100) / 100} h`}
               </div>
               <div className="w-48">{entry.message}</div>
               <div className="grid w-14 place-content-center">
                 <input
+                  className="scale-125"
                   id={`active-session-${index}`}
                   type="radio"
                   name="active-session"
+                  checked={selectedTaskIndex === index}
+                  onChange={() => {
+                    updateSelectedTaskIndex(index);
+                  }}
                 />
               </div>
             </div>
           ))}
+        </div>
+
+        <div className="mt-2" />
+        <div className="flex flex-row-reverse gap-2">
+          <label>Include break time</label>
+          <input
+            className="scale-150"
+            type="checkbox"
+            checked={showTimeWithBreaks}
+            onChange={(event) => setShowTimeWithBreaks(event.target.checked)}
+          />
         </div>
       </div>
     </Draggable>
