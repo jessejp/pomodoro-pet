9bfb32291a01190a2aada52d881c4b825c04f537
jessejp <jessepernila@gmail.com>
2023-09-11 18:21:49 +0300

    styles update for responsivity

--- src/interface/components/Menu.tsx
+++ src/interface/components/Menu.tsx
@@ -1,4 +1,6 @@
 import React, { useState, type PropsWithChildren } from "react";
+import clsx from "clsx";
+import { usePomodoro } from "../../utils/usePomodoro";
 
 type Tab = {
   icon: string;
@@ -10,9 +12,43 @@ interface MenuProps {
 }
 
 const Menu: React.FC<PropsWithChildren<MenuProps>> = ({ tabs }) => {
-  const [selectedTabIndex] = useState(0);
+  const { stop } = usePomodoro();
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
   return (
-    <div className="relative h-min">{tabs[selectedTabIndex].component}</div>
+    <div className="relative max-h-[40%] w-full">
+      <div className="absolute -top-12 left-2 flex items-center gap-1">
+        {tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={clsx(
+              "rounded-tl rounded-tr border-4 border-violet-700 bg-orangeFlavour px-3 pb-2 pt-2 text-center text-lg",
+              { "bg-gray-400": selectedTabIndex !== index },
+              { "border-b-transparent": selectedTabIndex === index }
+            )}
+            onClick={() => {
+              setSelectedTabIndex(index);
+            }}
+          >
+            {tab.icon}
+          </div>
+        ))}
+      </div>
+      <div className="flex h-full flex-col justify-between border-4 border-violet-700 bg-orangeFlavour px-4 py-4">
+        <div className="h-4/5 w-full max-w-7xl self-center">
+          {tabs[selectedTabIndex].component}
+        </div>
+        <div className="h-fit w-full max-w-2xl self-center border-t-2 border-violet-500 pt-2">
+          <button
+            className="w-fit rounded border-4 border-violet-700 bg-orangeFlavour px-4 py-2 text-2xl font-bold text-violet-700 hover:bg-orange-400 max-sm:text-lg"
+            onClick={() => {
+              stop();
+            }}
+          >
+            Stop
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
