a94edeea672e61e2adb407040ac8d3f7c487eb19
jessejp <jessepernila@gmail.com>
2024-01-13 16:43:48 +0200

    refactoring session log to use TanStack/table

--- src/interface/SessionLog.tsx
+++ src/interface/SessionLog.tsx
@@ -1,107 +1,83 @@
-import { useEffect, useState } from "react";
-import { useBoundStore } from "../store/useBoundStore";
+import { useState } from "react";
+import {
+  useReactTable,
+  createColumnHelper,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/react-table";
+
+type Log = {
+  task: string;
+  taskTimeSeconds: number;
+};
+
+const DUMMY_DATA: Log[] = [
+  {
+    task: "Writing documentation",
+    taskTimeSeconds: 7200, // 2h 0min
+  },
+  {
+    task: "code feat-style-session-log-v2",
+    taskTimeSeconds: 60 * 60 * 1 + 60 * 5, // 1h 5min
+  },
+  {
+    task: "Designing Session Log V2",
+    taskTimeSeconds: 60 * 30 + 35, // 30min 35sec
+  },
+];
+
+const columnHelper = createColumnHelper<Log>();
+
+const columns = [
+  columnHelper.accessor("task", {
+    header: "Task Name",
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor((row) => row.taskTimeSeconds, {
+    id: "taskTimeSeconds",
+    header: "Work Time",
+    cell: (info) => info.getValue(),
+  }),
+];
 
 const SessionLog = () => {
-  const {
-    sessionLog,
-    selectedTask,
-    newTaskMessage,
-    updateNewTaskMessage,
-    updateSelectedTaskIndex,
-  } = useBoundStore((state) => ({
-    sessionLog: state.sessionLog,
-    selectedTask: state.selectedTaskIndex,
-    newTaskMessage: state.newTaskMessage,
-    updateNewTaskMessage: state.updateNewTaskMessage,
-    updateSelectedTaskIndex: state.updateSelectedTaskIndex,
-  }));
-  const [taskName, setTaskName] = useState<string>(newTaskMessage);
+  const [data, setData] = useState<Log[]>(() => [...DUMMY_DATA]);
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      updateNewTaskMessage(taskName || "");
-    }, 800);
-
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [taskName, updateNewTaskMessage]);
+  console.log(table);
 
   return (
-    <>
-      <div className="flex h-5/6 w-full flex-col gap-3">
-        <div className="grid h-8 grid-cols-sessionlog thin:grid-cols-sessionlogMobile gap-3 text-sm">
-          <div>Length</div>
-          <div className="pl-1">Task Name</div>
-          <div>Active Task</div>
-        </div>
-        <div className="flex h-fit max-h-24 flex-col gap-3 overflow-y-scroll text-md scrollbar-thin scrollbar-thumb-tertiary-300 scrollbar-thumb-rounded-lg">
-          {sessionLog.map((entry, index) => {
-            const logTime = Math.round((entry.minutes / 60) * 100) / 100;
-            return (
-              <div
-                key={index}
-                className="grid grid-cols-sessionlog thin:grid-cols-sessionlogMobile items-center gap-3"
-              >
-                <div>{`${logTime === 0 ? "0.00" : logTime} h`}</div>
-                <div className="pl-1">{entry.message}</div>
-                <label htmlFor={`active-session-${index}`}>
-                  <input
-                    className="grid h-8 w-8 appearance-none
-                              place-content-center rounded-full bg-secondary-100 before:h-4 before:w-4 before:scale-0 before:rounded-full before:shadow-radioButtonInset before:shadow-tertiary-600 before:transition-transform before:content-[''] checked:before:scale-100"
-                    id={`active-session-${index}`}
-                    type="radio"
-                    name={`active-session-${index}`}
-                    checked={selectedTask === index}
-                    autoFocus={selectedTask === index}
-                    onChange={() => {
-                      updateSelectedTaskIndex(index);
-                    }}
-                  />
-                </label>
-              </div>
-            );
-          })}
-        </div>
-        <div className="grid h-8 grid-cols-sessionlog thin:grid-cols-sessionlogMobile gap-3 text-md">
-          <div>0.00&nbsp;h</div>
-          <input
-            className="h-8 rounded-lg bg-secondary-100 pl-1 thin:w-full"
-            value={taskName}
-            type="text"
-            placeholder="New Task"
-            onFocus={() => {
-              updateSelectedTaskIndex(-1);
-            }}
-            onChange={(event) => {
-              setTaskName(event.target.value);
-            }}
-          />
-          <label htmlFor="active-session--1">
-            <input
-              className="grid h-8 w-8 scale-100 appearance-none
-              place-content-center rounded-full bg-secondary-100 before:h-4 before:w-4 before:scale-0 before:rounded-full before:shadow-radioButtonInset before:shadow-tertiary-600 before:transition-transform before:content-[''] checked:before:scale-100"
-              id="active-session--1"
-              type="radio"
-              name="active-session"
-              checked={selectedTask === -1}
-              onChange={() => {
-                updateSelectedTaskIndex(-1);
-              }}
-            />
-          </label>
-        </div>
-      </div>
-      {/* <div className="flex h-1/6 flex-row-reverse items-center justify-start gap-2 py-2">
-        <label>Include break time</label>
-        <input
-          className="scale-150"
-          type="checkbox"
-          checked={showTimeWithBreaks}
-          onChange={(event) => setShowTimeWithBreaks(event.target.checked)}
-        />
-      </div> */}
-    </>
+    <table>
+      <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id}>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
--- src/store/useBoundStore.tsx
+++ src/store/useBoundStore.tsx
@@ -2,16 +2,13 @@ import { create } from "zustand";
 import type {
   CosmeticParams,
   PomodoroTimerParams,
-  SessionLogParams,
 } from "./types";
 import { PetCosmeticSlice } from "./slices/PetCosmeticSlice";
 import { PomodoroTimerSlice } from "./slices/PomodoroTimerSlice";
-import { SessionLogSlice } from "./slices/SessionLogSlice";
 
 export const useBoundStore = create<
-  CosmeticParams & PomodoroTimerParams & SessionLogParams
+  CosmeticParams & PomodoroTimerParams
 >()((...a) => ({
   ...PomodoroTimerSlice(...a),
   ...PetCosmeticSlice(...a),
-  ...SessionLogSlice(...a),
 }));
